616f780b443106ea53dc06881d22e38edcf04922
Ryan Graham <ryangrahamwpb@gmail.com>
2020-03-27 14:25:06 -0400

    Able to save data to db from front-end.

--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.http import require_GET, require_POST
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Album
+import json
 # from .forms import 
 from users.models import User
 from django.db.models import Q
@@ -13,8 +14,9 @@ import sys
 import pprint
 
 
+
 def search_album(request):
-    search_str = 'Blood Mountain'
+    search_str = 'Phil Collins'
 
     sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id='26b8ce1fe9a140f8a1867a55b7c0118e', client_secret='e8576337c58449e48270f0b90c1d8714'))
     result = sp.search(q=(search_str), type='album')
@@ -37,7 +39,14 @@ def rec_list(request):
 
 @csrf_exempt
 def new_album(request):
-   return render(request, 'core/new_album.html', ) 
+    if request.method == 'POST':
+       request.body
+       data = json.loads(request.body)
+       instance = Album(**data)
+    #    instance.users.set = request.user
+    #    print(instance.user)
+       instance.save()
+       return render(request, 'core/new_album.html',) 
 
 def site_search(request):
     search_str = request.GET.get('site-search')
--- static/main.js
+++ static/main.js
@@ -1,25 +1,30 @@
 let album = document.querySelector(".album");
-let imgSrc = document.querySelector(".album-cover").src;
-console.log("here", imgSrc);
+let album_cover = document.querySelector(".album-cover").src;
+
+// here we add an event listener to div class album
 album.addEventListener("click", function(e) {
-  console.log(e.target);
-  newData = [];
   data = e.target.innerText;
-  newData.push(data);
-  console.log(data);
-  fetch("/albums/new", {
-    method: "POST", // or 'PUT'
+  splitData = data.split("\n");
+  dataObject = {
+    album_cover,
+    name: splitData[0],
+    artist: splitData[1],
+    release_date: splitData[2]
+  };
+  console.log(dataObject);
+  fetch("/albums/new/", {
+    method: "POST",
     credentials: "include",
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(dataObject)
   })
     .then(response => response.json())
     .then(data => {
       return response.json();
     })
     .catch(error => {
-      console.error("Error:", error);
+      //   console.error("Error:", error);
     });
 });
