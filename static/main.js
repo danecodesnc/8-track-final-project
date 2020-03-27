86c1f71c12446abe249bf69547df8ccbdc562702
Ryan Graham <ryangrahamwpb@gmail.com>
2020-03-27 16:17:45 -0400

    Add for loop for all albums on c lick.

--- static/main.js
+++ static/main.js
@@ -1,30 +1,33 @@
-let album = document.querySelector(".album");
-let album_cover = document.querySelector(".album-cover").src;
+let albums = document.querySelectorAll(".album");
 
 // here we add an event listener to div class album
-album.addEventListener("click", function(e) {
-  data = e.target.innerText;
-  splitData = data.split("\n");
-  dataObject = {
-    album_cover,
-    name: splitData[0],
-    artist: splitData[1],
-    release_date: splitData[2]
-  };
-  console.log(dataObject);
-  fetch("/albums/new/", {
-    method: "POST",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(dataObject)
-  })
-    .then(response => response.json())
-    .then(data => {
-      return response.json();
+for (let album of albums) {
+  album.addEventListener("click", function(e) {
+    e.preventDefault();
+    let album_cover = document.querySelector(".album-cover").src;
+    data = e.target.innerText;
+    splitData = data.split("\n");
+    dataObject = {
+      album_cover,
+      name: splitData[0],
+      artist: splitData[1],
+      release_date: splitData[2]
+    };
+    console.log(dataObject);
+    fetch("/albums/new/", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(dataObject)
     })
-    .catch(error => {
-      //   console.error("Error:", error);
-    });
-});
+      .then(response => response.json())
+      .then(data => {
+        return response.json();
+      })
+      .catch(error => {
+        //   console.error("Error:", error);
+      });
+  });
+}
