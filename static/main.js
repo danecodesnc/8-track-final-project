0335b290810dcea8652f00c004ad861d3095c603
MFvanRossum <matissev@hotmail.com>
2020-03-30 13:29:05 -0400

    Adds album detail view. Doesn't quite work yet

--- static/main.js
+++ static/main.js
@@ -2,73 +2,64 @@ let span = document.querySelectorAll(".save-album");
 let albums = document.querySelectorAll(".album");
 let album_cover = document.querySelectorAll(".album-cover");
 
-function saveAlbum() {
-  for (let span of albums) {
-    span.addEventListener("click", function(e) {
-      e.preventDefault();
-      parent = this.closest("div");
-      data = this.closest("div").innerText;
-      album_cover = parent.querySelector(".album-cover").src;
-      splitData = data.split("\n");
-      dataObject = {
-        album_cover,
-        name: splitData[1],
-        artist: splitData[2],
-        release_date: splitData[3]
-      };
-      console.log(dataObject);
-      fetch("/albums/new/", {
-        method: "POST",
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(dataObject)
-      })
-        .then(response => response.json())
-        .then(data => {
-          return response.json();
+// function saveAlbum() {
+//   for (let span of albums) {
+//     span.addEventListener("click", function(e) {
+//       e.preventDefault();
+//       parent = this.closest("div");
+//       data = this.closest("div").innerText;
+//       album_cover = parent.querySelector(".album-cover").src;
+//       splitData = data.split("\n");
+//       dataObject = {
+//         album_cover,
+//         name: splitData[1],
+//         artist: splitData[2],
+//         release_date: splitData[3]
+//       };
+//       console.log(dataObject);
+//       fetch("/albums/new/", {
+//         method: "POST",
+//         credentials: "include",
+//         headers: {
+//           "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(dataObject)
+//       })
+//         .then(response => response.json())
+//         .then(data => {
+//           return response.json();
+//         })
+//         .catch(error => {
+//           //   console.error("Error:", error);
+//         });
+//     });
+//   }
+// }
+
+function album_detail() {
+    let album_titles = document.querySelectorAll('.album-title')
+    for (let title of album_titles) {
+        title.addEventListener("click", function(e) {
+            title = e.target.innerText
+            fetch("/album/detail/", {
+                credentials: 'include',
+                method: "POST",
+                headers: {
+                    "Content-Type": "text/html" 
+                },
+                body: JSON.stringify(title)
+            })
+            .then(response => response.text())
+            .then(data => {
+                response = title
+                return response
+            })
         })
-        .catch(error => {
-          //   console.error("Error:", error);
-        });
-    });
-  }
+    }
 }
 
 document.addEventListener("DOMContentLoaded", event => {
-  saveAlbum();
+//   saveAlbum();
+  album_detail();
 });
 
-// // here we add an event listener to div class album
-// for (let album of albums) {
-//   album.addEventListener("click", function(e) {
-//     e.preventDefault();
-//     let album_cover = document.querySelector(".album-cover").src;
-//     console.log(e.target.closest(album_cover));
-//     data = e.target.innerText;
-//     splitData = data.split("\n");
-//     dataObject = {
-//       album_cover,
-//       name: splitData[0],
-//       artist: splitData[1],
-//       release_date: splitData[2]
-//     };
-//     console.log(dataObject);
-//     fetch("/albums/new/", {
-//       method: "POST",
-//       credentials: "include",
-//       headers: {
-//         "Content-Type": "application/json"
-//       },
-//       body: JSON.stringify(dataObject)
-//     })
-//       .then(response => response.json())
-//       .then(data => {
-//         return response.json();
-//       })
-//       .catch(error => {
-//         //   console.error("Error:", error);
-//       });
-//   });
-// }
