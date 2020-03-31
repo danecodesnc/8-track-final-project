02b4c4e7ef436ba5cb2664aff493a9efde666394
Ryan Graham <ryangrahamwpb@gmail.com>
2020-03-31 17:07:47 -0400

    Fix for album and artisit uri.

--- static/main.js
+++ static/main.js
@@ -1,56 +1,45 @@
-let albumList = document.querySelector(".album-list");
+let albumSave = document.querySelector(".save-album");
 let albums = document.querySelectorAll(".album");
 let album_cover = document.querySelectorAll(".album-cover");
 
 function saveAlbum() {
-  albumList.addEventListener("click", function(e) {
-    e.preventDefault();
-    let album = e.target.querySelector(".album");
-    let dataUri = album.dataset.uri;
-    let artistUri = album.querySelector(".artist").dataset.artist;
-    album_cover = album.querySelector("img").src;
-    dataObject = {
-      album_cover,
-      name: album.querySelector(".album-title").textContent.trim(),
-      artist: album.querySelector(".artist").textContent.trim(),
-      release_date: album.querySelector(".album-year").textContent,
-      album_uri: dataUri,
-      artist_uri: artistUri
-    };
-    console.log(dataObject);
-    fetch("/albums/new/", {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(dataObject)
-    })
-      .then(response => response.json())
-      .then(data => {
-        return response.json();
+  for (let span of albums) {
+    span.addEventListener("click", function(e) {
+      e.preventDefault();
+      parent = this.closest("div");
+      data = this.closest("div").innerText;
+      album_cover = parent.querySelector(".album-cover").src;
+      let artist_uri = parent.querySelector(".artist").dataset.artist;
+      album_uri = parent.dataset.uri;
+      splitData = data.split("\n");
+      dataObject = {
+        album_cover,
+        name: splitData[1],
+        artist: splitData[2],
+        release_date: splitData[3],
+        album_uri: album_uri,
+        artist_uri: artist_uri
+      };
+      console.log(dataObject);
+      fetch("/albums/new/", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dataObject)
       })
-      .catch(error => {
-        //   console.error("Error:", error);
-      });
-  });
+        .then(response => response.json())
+        .then(data => {
+          return response.json();
+        })
+        .catch(error => {
+          //   console.error("Error:", error);
+        });
+    });
+  }
 }
 
 document.addEventListener("DOMContentLoaded", event => {
   saveAlbum();
-});
-
-let newRec = document.querySelector(".new-rec");
-
-let test = document.querySelector("#light-box");
-newRec.addEventListener("click", function(e) {
-  e.preventDefault();
-  test.style.display = "block";
-  test.classList.add("search");
-});
-
-let close = document.querySelector("#close");
-close.addEventListener("click", function(e) {
-  e.preventDefault();
-  test.style.display = "none";
 });
