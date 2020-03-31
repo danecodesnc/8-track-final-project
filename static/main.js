13b0f6bfde2bcbb77197863f8c29b5c7b72a3373
Ryan Graham <ryangrahamwpb@gmail.com>
2020-03-31 13:54:45 -0400

    URI for album and artist saves..

--- static/main.js
+++ static/main.js
@@ -2,53 +2,21 @@ let albumList = document.querySelector(".album-list");
 let albums = document.querySelectorAll(".album");
 let album_cover = document.querySelectorAll(".album-cover");
 
-// function saveAlbum() {
-//   for (let span of albums) {
-//     saveAlbum.addEventListener("click", function(e) {
-//       e.preventDefault();
-//       parent = this.closest("div");
-//       data = this.closest("div").innerText;
-//       album_cover = parent.querySelector(".album-cover").src;
-//       splitData = data.split("\n");
-//       console.log(splitData);
-//       dataObject = {
-//         album_cover,
-//         name: splitData[1],
-//         artist: splitData[2],
-//         release_date: splitData[3]
-//       };
-//       console.log(dataObject);
-//       fetch("/albums/new/", {
-//         method: "POST",
-//         credentials: "include",
-//         headers: {
-//           "Content-Type": "application/json"
-//         },
-//         body: JSON.stringify(dataObject)
-//       })
-//         .then(response => response.json())
-//         .then(data => {
-//           return response.json();
-//         })
-//         .catch(error => {
-//           //   console.error("Error:", error);
-//         });
-//     });
-//   }
-// }
-
 function saveAlbum() {
   albumList.addEventListener("click", function(e) {
     e.preventDefault();
     let info = e.target.querySelector(".album-info");
     let dataUri = info.dataset.uri;
+    let artistUri = document.querySelector(".artist").dataset.artist;
+    console.log("here", artistUri);
     album_cover = info.previousElementSibling.src;
     dataObject = {
       album_cover,
       name: info.querySelector(".album-title").textContent.trim(),
-      artist: info.querySelector(".artist").textContent,
+      artist: info.querySelector(".artist").textContent.trim(),
       release_date: info.querySelector(".album-year").textContent,
-      album_uri: dataUri
+      album_uri: dataUri,
+      artist_uri: artistUri
     };
     console.log(dataObject);
     fetch("/albums/new/", {
